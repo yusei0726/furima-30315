4a24734f9b835655157cc20ac58191aea02a8a97
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2020-12-17 19:35:38 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,           presence: true
+         validates :email,              presence: true
+         validates :encrypted_password, presence: true
+         validates :last_name_kanji,    presence: true
+         validates :first_name_kanji,   presence: true
+         validates :last_name_kana,     presence: true
+         validates :first_name_kana,    presence: true
+         validates :birthday,           presence: true
+
 end
