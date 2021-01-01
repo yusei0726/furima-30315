e646132039f17aff431e2a6ed4ed55275b81c66f
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-01 22:26:00 +0900

    Rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,           presence: true
-         validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" } do
-         end
-         with_options presence: true,   format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
-         validates :last_name_kanji,    presence: true
-         validates :first_name_kanji,   presence: true
-         validates :last_name_kana,     presence: true
-         validates :first_name_kana,    presence: true
-         end
-         validates :birthday,           presence: true
-
+  validates :nickname,           presence: true
+  validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上かつ英数字をそれぞれ含めてください' } do
+  end
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+    validates :last_name_kanji, presence: true
+    validates :first_name_kanji,   presence: true
+    validates :last_name_kana,     presence: true
+    validates :first_name_kana,    presence: true
+  end
+  validates :birthday, presence: true
 end
