7e01fcc98b7d46765164421ecb2cc1762ad94620
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-10 19:43:57 +0900

    itemの単体モデルテストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    title               {Faker::Name.name}
+    concept             {Faker::Lorem.sentence}
+    price               {3000}
+    category_id         {2}
+    prefecture_id       {2}
+    product_status_id   {2}
+    shipping_charge_id  {2}
+    shipping_day_id     {2}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
