7d7976e703fcadc5e251e92e9bf95b553216f7cc
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-11 12:47:28 +0900

    rubocopによる修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーの情報が[---]では出品できない' do
       @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category must be other than 1")
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
     it '商品の状態についての情報が空では出品できない' do
       @item.product_status_id = nil
@@ -44,7 +44,7 @@ RSpec.describe Item, type: :model do
     it '商品の状態についての情報が[---]では出品できない' do
       @item.product_status_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Product status must be other than 1")
+      expect(@item.errors.full_messages).to include('Product status must be other than 1')
     end
     it '配送料の負担についての情報が空では出品できない' do
       @item.shipping_charge_id = nil
@@ -54,7 +54,7 @@ RSpec.describe Item, type: :model do
     it '配送料の負担についての情報が[---]では出品できない' do
       @item.shipping_charge_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+      expect(@item.errors.full_messages).to include('Shipping charge must be other than 1')
     end
     it '発送元の地域についての情報が空では出品できない' do
       @item.prefecture_id = nil
@@ -64,7 +64,7 @@ RSpec.describe Item, type: :model do
     it '発送元の地域についての情報が[---]では出品できない' do
       @item.prefecture_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
     end
     it '発送までの日数についての情報が空では出品できない' do
       @item.shipping_day_id = nil
@@ -74,7 +74,7 @@ RSpec.describe Item, type: :model do
     it '発送までの日数についての情報が空では出品できない' do
       @item.shipping_day_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
+      expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
     end
     it '価格についての情報が空では出品できない' do
       @item.price = nil
