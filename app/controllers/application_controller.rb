91789021396fb63e6c2aeb8f2befe01c50d2c735
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-12 22:55:19 +0900

    pullrequestによる修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_index, only: :edit
+  
 
   private
 
