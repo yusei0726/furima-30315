821265ed4861265d1f69aee6769e7b50f9e0220e
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-12 21:19:06 +0900

    itemsコントローラーのアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :concept, :category_id, :product_status_id, :shipping_charge_id, :shipping_day_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    if user_signed_in?
+      redirect_to action: :index unless current_user.id == @item.user_id
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
