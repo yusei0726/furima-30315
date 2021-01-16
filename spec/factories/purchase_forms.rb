425e1d72365b0f3e7c3304852fd54c82dde39aea
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-16 21:27:48 +0900

    purchase_formモデルの単体テストコード実装

---
+++ spec/factories/purchase_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_form do 
+    postal_code     { '123-4567' }
+    prefecture_id   { 10 }
+    municiparity    { '東京都港区' }
+    house_number    { '旭町1-23-4' }
+    building_name   { '朝日ビル' }
+    phone_number    { '08012345678' }
+  end
+end
